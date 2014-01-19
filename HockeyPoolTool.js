502ec01a19959fe305faf2b7ed546a4185f64e65
tweeres04 <tweeres04@gmail.com>
2014-01-19 00:28:50 -0800

    Code reorganization

--- HockeyPoolTool.js
+++ HockeyPoolTool.js
@@ -1,24 +1,3 @@
-var ViewModel = function () {
-    var me = this;
-
-    me.views = [ { name: "my-team", viewModel: MyTeamViewModel, label: "My Team" }, { name: "stats", viewModel: StatsViewModel, label: "Stats" } ];
-    me.currentView = ko.observable(me.views[0]);
-
-    me.allPlayers = ko.observableArray();
-
-    me.allTeams = ko.observableArray();
-
-    me.currentViewModel = new MyTeamViewModel({ parent: me });
-
-    me.currentView.subscribe(function(newView){
-        me.currentViewModel = new newView.viewModel({ parent: me });
-    });
-
-    $(document).ajaxError(function(event, request, settings) {
-        showErrorMessage("danger", 'Error requesting <span class="alert-link">' + settings.url + "</span>");
-    });
-};
-
 function showErrorMessage(type, message){
     var alert = $("<div>").addClass("alert alert-" + type);
     alert.append('<button type="button" class="close" data-dismiss="alert">&times;</button>');
@@ -26,33 +5,36 @@ function showErrorMessage(type, message){
     $(".container").prepend(alert);
 }
 
-function StatsViewModel(options){
+var HockeyPoolToolViewModel = function () {
     var me = this;
-    me.parent = options.parent;
-}
 
-function MyTeamViewModel(options){
-    var me = this;
-    me.parent = options.parent;
+    me.views = [ { name: "my-team", viewModel: MyTeamViewModel, label: "My Team" }, { name: "stats", viewModel: StatsViewModel, label: "Stats" } ];
+    me.currentView = ko.observable(me.views[0]);
 
-    me.resizeMyTeamPanel = function () {
-        $(".my-team").height($(".players-list").height());
-    };
-
-    me.selectedTeam = ko.observable({ code: "Hi", name: "Please select a team" });
-    me.selectedTeam.subscribe(function (newValue) {
-        me.resizeMyTeamPanel();
-    });
-    me.selectedPlayer = ko.observable("");
-
+    me.allPlayers = ko.observableArray();
+    me.allTeams = ko.observableArray();
     me.myTeam = ko.observableArray();
+    me.currentViewModel = ko.observable();
 
-    me.loadData = function (callback) {
+    me.currentView.subscribe(function(newView){
+        me.currentViewModel = new newView.viewModel({ parent: me });
+    });
+
+    $.ajaxSetup({ contentType: "application/json" });
+
+    $(document).ajaxError(function(event, request, settings) {
+        showErrorMessage("danger", 'Error requesting <span class="alert-link">' + settings.url + "</span>");
+    });
+
+    loadData(function(){
+        me.currentViewModel(new MyTeamViewModel({ parent: me }));
+    });
+
+    function loadData(callback) {
         $.getJSON("/teams", function (teams) {
-            me.parent.allTeams(teams);
-            me.selectedTeam(me.parent.allTeams()[0]);
+            me.allTeams(teams);
             $.getJSON("/players", function (players) {
-                me.parent.allPlayers(players);
+                me.allPlayers(players);
                 $.getJSON("/myteam", function (team) {
                     me.myTeam(team);
 
@@ -63,9 +45,9 @@ function MyTeamViewModel(options){
                     });
 
                     for (var i in me.myTeam()) {
-                        for(var j in me.parent.allPlayers()){
-                            if(me.myTeam()[i].Name == me.parent.allPlayers()[j].Name){
-                                me.parent.allPlayers.remove(me.parent.allPlayers()[j]);
+                        for(var j in me.allPlayers()){
+                            if(me.myTeam()[i].Name == me.allPlayers()[j].Name){
+                                me.allPlayers.remove(me.allPlayers()[j]);
                                 break;
                             }
                         }
@@ -75,47 +57,59 @@ function MyTeamViewModel(options){
             });
         });
     };
+};
 
-    me.init = function () {
-        $.ajaxSetup({ contentType: "application/json" });
+function StatsViewModel(options){
+    var me = this;
+    me.parent = options.parent;
 
-        me.loadData(afterLoadData);
-        
-        function afterLoadData() {
-            me.resizeMyTeamPanel();
-            setUpDragAndDrop();
-        }
-
-        function setUpDragAndDrop() {
-            $(".player, .my-team-player").draggable({ revert: true, zIndex: 10 });
-            $(".my-team").droppable({
-                accept: ".player",
-                tolerance: "pointer",
-                drop: function (event, ui) {
-                    var player = ko.dataFor(ui.draggable[0]);
-                    me.parent.allPlayers.remove(player);
-                    me.myTeam.push(player);
-                    $(".my-team-player").draggable({ revert: true });
-                }
-            });
-            $("body").droppable({
-                accept: ".my-team-player",
-                tolerance: "pointer",
-                drop: function (event, ui) {
-                    var player = ko.dataFor(ui.draggable[0]);
-                    me.parent.allPlayers.push(player);
-                    me.myTeam.remove(player);
-                    $(".player").draggable({ revert: true });
-                }
-            });
-
-            $(".my-team").on("drop", function (event, ui) {
-                event.stopPropagation();
-            });
-        }
-    };
-
-    me.init();
+    me.headers = Object.keys(me.parent.allPlayers()[0]);
 }
 
-ko.applyBindings(new ViewModel());
+function MyTeamViewModel(options){
+    var me = this;
+    me.parent = options.parent;
+
+    me.selectedTeam = ko.observable();
+    me.selectedTeam.subscribe(function (newValue) {
+        resizeMyTeamPanel();
+    });
+    me.selectedTeam(me.parent.allTeams()[0]);
+    //me.selectedPlayer = ko.observable("");
+
+    function resizeMyTeamPanel() {
+        $(".my-team").height($(".players-list").height());
+    };
+
+    function setUpDragAndDrop() {
+        $(".player, .my-team-player").draggable({ revert: true, zIndex: 10 });
+
+        $(".my-team").droppable({
+            accept: ".player",
+            tolerance: "pointer",
+            drop: function (event, ui) {
+                var player = ko.dataFor(ui.draggable[0]);
+                me.parent.allPlayers.remove(player);
+                me.myTeam.push(player);
+                $(".my-team-player").draggable({ revert: true });
+            }
+        });
+
+        $("body").droppable({
+            accept: ".my-team-player",
+            tolerance: "pointer",
+            drop: function (event, ui) {
+                var player = ko.dataFor(ui.draggable[0]);
+                me.parent.allPlayers.push(player);
+                me.myTeam.remove(player);
+                $(".player").draggable({ revert: true });
+            }
+        });
+
+        $(".my-team").on("drop", function (event, ui) {
+            event.stopPropagation();
+        });
+    }
+}
+
+ko.applyBindings(new HockeyPoolToolViewModel());
